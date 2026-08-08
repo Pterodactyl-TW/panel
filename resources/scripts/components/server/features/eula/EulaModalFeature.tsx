198fae605555a091adc515d9ef7225cde8842ff0
Kevin <0918150353yuiop@gmail.com>
2026-08-08 17:28:17 +0800

    i18n: 翻譯 EULA 同意彈窗的使用者介面文字

--- resources/scripts/components/server/features/eula/EulaModalFeature.tsx
+++ resources/scripts/components/server/features/eula/EulaModalFeature.tsx
@@ -65,9 +65,9 @@ const EulaModalFeature = () => {
             showSpinnerOverlay={loading}
         >
             <FlashMessageRender key={'feature:eula'} css={tw`mb-4`} />
-            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>Accept Minecraft&reg; EULA</h2>
+            <h2 css={tw`text-2xl mb-4 text-neutral-100`}>接受 Minecraft&reg; EULA</h2>
             <p css={tw`text-neutral-200`}>
-                By pressing {'"I Accept"'} below you are indicating your agreement to the&nbsp;
+                點選下方的「我接受」即表示你同意&nbsp;
                 <a
                     target={'_blank'}
                     css={tw`text-primary-300 underline transition-colors duration-150 hover:text-primary-400`}
@@ -76,14 +76,14 @@ const EulaModalFeature = () => {
                 >
                     Minecraft&reg; EULA
                 </a>
-                .
+                。
             </p>
             <div css={tw`mt-8 sm:flex items-center justify-end`}>
                 <Button isSecondary onClick={() => setVisible(false)} css={tw`w-full sm:w-auto border-transparent`}>
-                    Cancel
+                    取消
                 </Button>
                 <Button onClick={onAcceptEULA} css={tw`mt-4 sm:mt-0 sm:ml-4 w-full sm:w-auto`}>
-                    I Accept
+                    我接受
                 </Button>
             </div>
         </Modal>
