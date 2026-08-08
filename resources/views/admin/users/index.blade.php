236f378395b5a2fd27cda0ed5c09535d3f28f922
Kevin <0918150353yuiop@gmail.com>
2026-08-08 10:02:33 +0800

    i18n: 翻譯 admin/users 的 Blade 樣板

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin')
 
 @section('title')
-    List Users
+    使用者清單
 @endsection
 
 @section('content-header')
-    <h1>Users<small>All registered users on the system.</small></h1>
+    <h1>使用者<small>系統中所有已註冊的使用者。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Users</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li class="active">使用者</li>
     </ol>
 @endsection
 
@@ -17,14 +17,14 @@
     <div class="col-xs-12">
         <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">User List</h3>
+                <h3 class="box-title">使用者清單</h3>
                 <div class="box-tools search01">
                     <form action="{{ route('admin.users') }}" method="GET">
                         <div class="input-group input-group-sm">
-                            <input type="text" name="filter[email]" class="form-control pull-right" value="{{ request()->input('filter.email') }}" placeholder="Search">
+                            <input type="text" name="filter[email]" class="form-control pull-right" value="{{ request()->input('filter.email') }}" placeholder="搜尋">
                             <div class="input-group-btn">
                                 <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                                <a href="{{ route('admin.users.new') }}"><button type="button" class="btn btn-sm btn-primary" style="border-radius: 0 3px 3px 0;margin-left:-1px;">Create New</button></a>
+                                <a href="{{ route('admin.users.new') }}"><button type="button" class="btn btn-sm btn-primary" style="border-radius: 0 3px 3px 0;margin-left:-1px;">建立</button></a>
                             </div>
                         </div>
                     </form>
@@ -35,12 +35,12 @@
                     <thead>
                         <tr>
                             <th>ID</th>
-                            <th>Email</th>
-                            <th>Client Name</th>
-                            <th>Username</th>
-                            <th class="text-center">2FA</th>
-                            <th class="text-center"><span data-toggle="tooltip" data-placement="top" title="Servers that this user is marked as the owner of.">Servers Owned</span></th>
-                            <th class="text-center"><span data-toggle="tooltip" data-placement="top" title="Servers that this user can access because they are marked as a subuser.">Can Access</span></th>
+                            <th>電子郵件</th>
+                            <th>客戶名稱</th>
+                            <th>使用者名稱</th>
+                            <th class="text-center">雙重驗證</th>
+                            <th class="text-center"><span data-toggle="tooltip" data-placement="top" title="此使用者被標記為擁有者的伺服器。">擁有的伺服器</span></th>
+                            <th class="text-center"><span data-toggle="tooltip" data-placement="top" title="此使用者因被標記為子使用者而可以存取的伺服器。">可存取</span></th>
                             <th></th>
                         </tr>
                     </thead>
--- resources/views/admin/users/new.blade.php
+++ resources/views/admin/users/new.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin')
 
 @section('title')
-    Create User
+    建立使用者
 @endsection
 
 @section('content-header')
-    <h1>Create User<small>Add a new user to the system.</small></h1>
+    <h1>建立使用者<small>在系統中新增一位使用者。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li><a href="{{ route('admin.users') }}">Users</a></li>
-        <li class="active">Create</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li><a href="{{ route('admin.users') }}">使用者</a></li>
+        <li class="active">建立</li>
     </ol>
 @endsection
 
@@ -19,65 +19,65 @@
         <div class="col-md-6">
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Identity</h3>
+                    <h3 class="box-title">身分資訊</h3>
                 </div>
                 <div class="box-body">
                     <div class="form-group">
-                        <label for="email" class="control-label">Email</label>
+                        <label for="email" class="control-label">電子郵件</label>
                         <div>
                             <input type="text" autocomplete="off" name="email" value="{{ old('email') }}" class="form-control" />
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="username" class="control-label">Username</label>
+                        <label for="username" class="control-label">使用者名稱</label>
                         <div>
                             <input type="text" autocomplete="off" name="username" value="{{ old('username') }}" class="form-control" />
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="name_first" class="control-label">Client First Name</label>
+                        <label for="name_first" class="control-label">客戶名字</label>
                         <div>
                             <input type="text" autocomplete="off" name="name_first" value="{{ old('name_first') }}" class="form-control" />
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="name_last" class="control-label">Client Last Name</label>
+                        <label for="name_last" class="control-label">客戶姓氏</label>
                         <div>
                             <input type="text" autocomplete="off" name="name_last" value="{{ old('name_last') }}" class="form-control" />
                         </div>
                     </div>
                     <div class="form-group">
-                        <label class="control-label">Default Language</label>
+                        <label class="control-label">預設語言</label>
                         <div>
                             <select name="language" class="form-control">
                                 @foreach($languages as $key => $value)
                                     <option value="{{ $key }}" @if(config('app.locale') === $key) selected @endif>{{ $value }}</option>
                                 @endforeach
                             </select>
-                            <p class="text-muted"><small>The default language to use when rendering the Panel for this user.</small></p>
+                            <p class="text-muted"><small>為此使用者渲染 Panel 時使用的預設語言。</small></p>
                         </div>
                     </div>
                 </div>
                 <div class="box-footer">
                     {!! csrf_field() !!}
-                    <input type="submit" value="Create User" class="btn btn-success btn-sm">
+                    <input type="submit" value="建立使用者" class="btn btn-success btn-sm">
                 </div>
             </div>
         </div>
         <div class="col-md-6">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Permissions</h3>
+                    <h3 class="box-title">權限</h3>
                 </div>
                 <div class="box-body">
                     <div class="form-group col-md-12">
-                        <label for="root_admin" class="control-label">Administrator</label>
+                        <label for="root_admin" class="control-label">管理員</label>
                         <div>
                             <select name="root_admin" class="form-control">
                                 <option value="0">@lang('strings.no')</option>
                                 <option value="1">@lang('strings.yes')</option>
                             </select>
-                            <p class="text-muted"><small>Setting this to 'Yes' gives a user full administrative access.</small></p>
+                            <p class="text-muted"><small>將此設為「是」將授予使用者完整的管理權限。</small></p>
                         </div>
                     </div>
                 </div>
@@ -86,15 +86,15 @@
         <div class="col-md-6">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Password</h3>
+                    <h3 class="box-title">密碼</h3>
                 </div>
                 <div class="box-body">
                     <div class="alert alert-info">
-                        <p>Providing a user password is optional. New user emails prompt users to create a password the first time they login. If a password is provided here you will need to find a different method of providing it to the user.</p>
+                        <p>提供使用者密碼是可選的。新使用者的電子郵件會提示使用者在首次登入時建立密碼。若在此提供了密碼，你將需要另尋方式將其提供給使用者。</p>
                     </div>
                     <div id="gen_pass" class=" alert alert-success" style="display:none;margin-bottom: 10px;"></div>
                     <div class="form-group">
-                        <label for="pass" class="control-label">Password</label>
+                        <label for="pass" class="control-label">密碼</label>
                         <div>
                             <input type="password" name="password" class="form-control" />
                         </div>
@@ -117,7 +117,7 @@
                     'X-CSRF-TOKEN': '{{ csrf_token() }}'
                },
                 success: function(data) {
-                    $("#gen_pass").html('<strong>Generated Password:</strong> ' + data).slideDown();
+                    $("#gen_pass").html('<strong>已產生密碼：</strong> ' + data).slideDown();
                     $('input[name="password"], input[name="password_confirmation"]').val(data);
                     return false;
                 }
