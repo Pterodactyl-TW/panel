57031bebc3fd871c86b90469b7a60924d7127f26
Kevin <0918150353yuiop@gmail.com>
2026-08-08 10:01:07 +0800

    i18n: 翻譯 admin/settings 的 Blade 樣板

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -2,14 +2,14 @@
 @include('partials/admin.settings.nav', ['activeTab' => 'basic'])
 
 @section('title')
-    Settings
+    設定
 @endsection
 
 @section('content-header')
-    <h1>Panel Settings<small>Configure Pterodactyl to your liking.</small></h1>
+    <h1>Panel 設定<small>依你的喜好配置 Pterodactyl。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Settings</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li class="active">設定</li>
     </ol>
 @endsection
 
@@ -19,54 +19,54 @@
         <div class="col-xs-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Panel Settings</h3>
+                    <h3 class="box-title">Panel 設定</h3>
                 </div>
                 <form action="{{ route('admin.settings') }}" method="POST">
                     <div class="box-body">
                         <div class="row">
                             <div class="form-group col-md-4">
-                                <label class="control-label">Company Name</label>
+                                <label class="control-label">公司名稱</label>
                                 <div>
                                     <input type="text" class="form-control" name="app:name" value="{{ old('app:name', config('app.name')) }}" />
-                                    <p class="text-muted"><small>This is the name that is used throughout the panel and in emails sent to clients.</small></p>
+                                    <p class="text-muted"><small>此名稱將使用於整個 Panel 及寄送給客戶的電子郵件中。</small></p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <label class="control-label">Require 2-Factor Authentication</label>
+                                <label class="control-label">要求雙重驗證</label>
                                 <div>
                                     <div class="btn-group" data-toggle="buttons">
                                         @php
                                             $level = old('pterodactyl:auth:2fa_required', config('pterodactyl.auth.2fa_required'));
                                         @endphp
                                         <label class="btn btn-primary @if ($level == 0) active @endif">
-                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="0" @if ($level == 0) checked @endif> Not Required
+                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="0" @if ($level == 0) checked @endif> 不要求
                                         </label>
                                         <label class="btn btn-primary @if ($level == 1) active @endif">
-                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="1" @if ($level == 1) checked @endif> Admin Only
+                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="1" @if ($level == 1) checked @endif> 僅限管理員
                                         </label>
                                         <label class="btn btn-primary @if ($level == 2) active @endif">
-                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="2" @if ($level == 2) checked @endif> All Users
+                                            <input type="radio" name="pterodactyl:auth:2fa_required" autocomplete="off" value="2" @if ($level == 2) checked @endif> 所有使用者
                                         </label>
                                     </div>
-                                    <p class="text-muted"><small>If enabled, any account falling into the selected grouping will be required to have 2-Factor authentication enabled to use the Panel.</small></p>
+                                    <p class="text-muted"><small>若啟用，屬於所選群組的所有帳號都必須啟用雙重驗證才能使用 Panel。</small></p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <label class="control-label">Default Language</label>
+                                <label class="control-label">預設語言</label>
                                 <div>
                                     <select name="app:locale" class="form-control">
                                         @foreach($languages as $key => $value)
                                             <option value="{{ $key }}" @if(config('app.locale') === $key) selected @endif>{{ $value }}</option>
                                         @endforeach
                                     </select>
-                                    <p class="text-muted"><small>The default language to use when rendering UI components.</small></p>
+                                    <p class="text-muted"><small>渲染 UI 元件時使用的預設語言。</small></p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="box-footer">
                         {!! csrf_field() !!}
-                        <button type="submit" name="_method" value="PATCH" class="btn btn-sm btn-primary pull-right">Save</button>
+                        <button type="submit" name="_method" value="PATCH" class="btn btn-sm btn-primary pull-right">儲存</button>
                     </div>
                 </form>
             </div>
--- resources/views/admin/settings/mail.blade.php
+++ resources/views/admin/settings/mail.blade.php
@@ -2,14 +2,14 @@
 @include('partials/admin.settings.nav', ['activeTab' => 'mail'])
 
 @section('title')
-    Mail Settings
+    郵件設定
 @endsection
 
 @section('content-header')
-    <h1>Mail Settings<small>Configure how Pterodactyl should handle sending emails.</small></h1>
+    <h1>郵件設定<small>配置 Pterodactyl 應如何處理寄送電子郵件。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Settings</li>
+        <li><a href="{{ route('admin.index') }}">管理</a></li>
+        <li class="active">設定</li>
     </ol>
 @endsection
 
@@ -19,14 +19,14 @@
         <div class="col-xs-12">
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Email Settings</h3>
+                    <h3 class="box-title">電子郵件設定</h3>
                 </div>
                 @if($disabled)
                     <div class="box-body">
                         <div class="row">
                             <div class="col-xs-12">
                                 <div class="alert alert-info no-margin-bottom">
-                                    This interface is limited to instances using SMTP as the mail driver. Please either use <code>php artisan p:environment:mail</code> command to update your email settings, or set <code>MAIL_DRIVER=smtp</code> in your environment file.
+                                    此介面僅限於使用 SMTP 作為郵件驅動程式的實例。請使用 <code>php artisan p:environment:mail</code> 指令更新你的電子郵件設定，或在你的環境檔案中設定 <code>MAIL_DRIVER=smtp</code>。
                                 </div>
                             </div>
                         </div>
@@ -36,62 +36,62 @@
                         <div class="box-body">
                             <div class="row">
                                 <div class="form-group col-md-6">
-                                    <label class="control-label">SMTP Host</label>
+                                    <label class="control-label">SMTP 主機</label>
                                     <div>
                                         <input required type="text" class="form-control" name="mail:mailers:smtp:host" value="{{ old('mail:mailers:smtp:host', config('mail.mailers.smtp.host')) }}" />
-                                        <p class="text-muted small">Enter the SMTP server address that mail should be sent through.</p>
+                                        <p class="text-muted small">輸入用於寄送郵件的 SMTP 伺服器位址。</p>
                                     </div>
                                 </div>
                                 <div class="form-group col-md-2">
-                                    <label class="control-label">SMTP Port</label>
+                                    <label class="control-label">SMTP 連接埠</label>
                                     <div>
                                         <input required type="number" class="form-control" name="mail:mailers:smtp:port" value="{{ old('mail:mailers:smtp:port', config('mail.mailers.smtp.port')) }}" />
-                                        <p class="text-muted small">Enter the SMTP server port that mail should be sent through.</p>
+                                        <p class="text-muted small">輸入用於寄送郵件的 SMTP 伺服器連接埠。</p>
                                     </div>
                                 </div>
                                 <div class="form-group col-md-4">
-                                    <label class="control-label">Encryption</label>
+                                    <label class="control-label">加密方式</label>
                                     <div>
                                         @php
                                             $encryption = old('mail:mailers:smtp:encryption', config('mail.mailers.smtp.encryption'));
                                         @endphp
                                         <select name="mail:mailers:smtp:encryption" class="form-control">
-                                            <option value="" @if($encryption === '') selected @endif>None</option>
-                                            <option value="tls" @if($encryption === 'tls') selected @endif>Transport Layer Security (TLS)</option>
-                                            <option value="ssl" @if($encryption === 'ssl') selected @endif>Secure Sockets Layer (SSL)</option>
+                                            <option value="" @if($encryption === '') selected @endif>無</option>
+                                            <option value="tls" @if($encryption === 'tls') selected @endif>傳輸層安全性 (TLS)</option>
+                                            <option value="ssl" @if($encryption === 'ssl') selected @endif>安全通訊端層 (SSL)</option>
                                         </select>
-                                        <p class="text-muted small">Select the type of encryption to use when sending mail.</p>
+                                        <p class="text-muted small">選擇寄送郵件時要使用的加密方式。</p>
                                     </div>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label class="control-label">Username <span class="field-optional"></span></label>
+                                    <label class="control-label">使用者名稱 <span class="field-optional"></span></label>
                                     <div>
                                         <input type="text" class="form-control" name="mail:mailers:smtp:username" value="{{ old('mail:mailers:smtp:username', config('mail.mailers.smtp.username')) }}" />
-                                        <p class="text-muted small">The username to use when connecting to the SMTP server.</p>
+                                        <p class="text-muted small">連接到 SMTP 伺服器時使用的使用者名稱。</p>
                                     </div>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label class="control-label">Password <span class="field-optional"></span></label>
+                                    <label class="control-label">密碼 <span class="field-optional"></span></label>
                                     <div>
                                         <input type="password" class="form-control" name="mail:mailers:smtp:password"/>
-                                        <p class="text-muted small">The password to use in conjunction with the SMTP username. Leave blank to continue using the existing password. To set the password to an empty value enter <code>!e</code> into the field.</p>
+                                        <p class="text-muted small">搭配 SMTP 使用者名稱使用的密碼。留空則繼續使用現有密碼。若要將密碼設為空值，請在此欄位輸入 <code>!e</code>。</p>
                                     </div>
                                 </div>
                             </div>
                             <div class="row">
                                 <hr />
                                 <div class="form-group col-md-6">
-                                    <label class="control-label">Mail From</label>
+                                    <label class="control-label">寄件者信箱</label>
                                     <div>
                                         <input required type="email" class="form-control" name="mail:from:address" value="{{ old('mail:from:address', config('mail.from.address')) }}" />
-                                        <p class="text-muted small">Enter an email address that all outgoing emails will originate from.</p>
+                                        <p class="text-muted small">輸入所有寄出郵件的來源電子郵件地址。</p>
                                     </div>
                                 </div>
                                 <div class="form-group col-md-6">
-                                    <label class="control-label">Mail From Name <span class="field-optional"></span></label>
+                                    <label class="control-label">寄件者名稱 <span class="field-optional"></span></label>
                                     <div>
                                         <input type="text" class="form-control" name="mail:from:name" value="{{ old('mail:from:name', config('mail.from.name')) }}" />
-                                        <p class="text-muted small">The name that emails should appear to come from.</p>
+                                        <p class="text-muted small">郵件顯示的寄件者名稱。</p>
                                     </div>
                                 </div>
                             </div>
@@ -99,8 +99,8 @@
                         <div class="box-footer">
                             {{ csrf_field() }}
                             <div class="pull-right">
-                                <button type="button" id="testButton" class="btn btn-sm btn-success">Test</button>
-                                <button type="button" id="saveButton" class="btn btn-sm btn-primary">Save</button>
+                                <button type="button" id="testButton" class="btn btn-sm btn-success">測試</button>
+                                <button type="button" id="saveButton" class="btn btn-sm btn-primary">儲存</button>
                             </div>
                         </div>
                     </form>
@@ -130,17 +130,17 @@
                 }),
                 headers: { 'X-CSRF-Token': $('input[name="_token"]').val() }
             }).fail(function (jqXHR) {
-                showErrorDialog(jqXHR, 'save');
+                showErrorDialog(jqXHR, '儲存');
             });
         }
 
         function testSettings() {
             swal({
                 type: 'info',
-                title: 'Test Mail Settings',
-                text: 'Click "Test" to begin the test.',
+                title: '測試郵件設定',
+                text: '點擊「測試」以開始測試。',
                 showCancelButton: true,
-                confirmButtonText: 'Test',
+                confirmButtonText: '測試',
                 closeOnConfirm: false,
                 showLoaderOnConfirm: true
             }, function () {
@@ -149,11 +149,11 @@
                     url: '/admin/settings/mail/test',
                     headers: { 'X-CSRF-TOKEN': $('input[name="_token"]').val() }
                 }).fail(function (jqXHR) {
-                    showErrorDialog(jqXHR, 'test');
+                    showErrorDialog(jqXHR, '測試');
                 }).done(function () {
                     swal({
-                        title: 'Success',
-                        text: 'The test message was sent successfully.',
+                        title: '成功',
+                        text: '測試郵件已成功寄出。',
                         type: 'success'
                     });
                 });
@@ -180,8 +180,8 @@
             }
 
             swal({
-                title: 'Whoops!',
-                text: 'An error occurred while attempting to ' + verb + ' mail settings: ' + errorText,
+                title: '糟糕！',
+                text: '嘗試' + verb + '郵件設定時發生錯誤：' + errorText,
                 type: 'error'
             });
         }
@@ -191,8 +191,8 @@
             $('#saveButton').on('click', function () {
                 saveSettings().done(function () {
                     swal({
-                        title: 'Success',
-                        text: 'Mail settings have been updated successfully and the queue worker was restarted to apply these changes.',
+                        title: '成功',
+                        text: '郵件設定已成功更新，且佇列工作程序已重新啟動以套用這些變更。',
                         type: 'success'
                     });
                 });
