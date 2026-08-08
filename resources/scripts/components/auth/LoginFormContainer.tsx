e6809fcdfe679ffd083585e2899ca7945d4488c5
Kevin <0918150353yuiop@gmail.com>
2026-08-08 18:28:22 +0800

    i18n: 統一登入頁面頁尾的 Pterodactyl-TW 版權格式
    改用與 PageContentBlock.tsx 一致的雙行版權格式：Pterodactyl-TW 台灣翻譯團隊版權聲明 + 原始開發者 Pterodactyl Software 的引用。

--- resources/scripts/components/auth/LoginFormContainer.tsx
+++ resources/scripts/components/auth/LoginFormContainer.tsx
@@ -41,15 +41,27 @@ export default forwardRef<HTMLFormElement, Props>(({ title, ...props }, ref) => 
             </div>
         </Form>
         <p css={tw`text-center text-neutral-500 text-xs mt-4`}>
-            &copy; 2015 - {new Date().getFullYear()}&nbsp;
+            Copyright &copy; 2026 - {new Date().getFullYear()}&nbsp;
             <a
                 rel={'noopener nofollow noreferrer'}
                 href={'https://pterodactyl.tw'}
                 target={'_blank'}
                 css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
             >
+                Pterodactyl-TW 臺灣翻譯團隊
+            </a>
+            . All Rights Reserved.
+            <br />
+            Based on software originally developed by&nbsp;
+            <a
+                rel={'noopener nofollow noreferrer'}
+                href={'https://pterodactyl.io'}
+                target={'_blank'}
+                css={tw`no-underline text-neutral-500 hover:text-neutral-300`}
+            >
                 Pterodactyl Software
             </a>
+            .
         </p>
     </Container>
 ));
