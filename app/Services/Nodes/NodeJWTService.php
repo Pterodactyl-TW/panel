7383763b68553dc519786110ff8997ab672fcb48
Kevin <0918150353yuiop@gmail.com>
2026-08-13 16:08:03 +0800

    style: 移除 NodeJWTService.php 多餘的空白行結尾字元（php-cs-fixer）

--- app/Services/Nodes/NodeJWTService.php
+++ app/Services/Nodes/NodeJWTService.php
@@ -97,7 +97,7 @@ class NodeJWTService
         }
 
         Assert::notEmpty($this->scopes, '必須提供至少一個權限範圍（scope）才能產生 JWT。');
-        
+
         $builder = $builder->withClaim('scope', implode(' ', array_map(fn ($scope) => $scope->value, $this->scopes)));
 
         if (!is_null($this->user)) {
